f67d7f5e6b0d4a4caa1e9dc68f9ec4611ee80dc0
AJ ONeal <awesome@coolaj86.com>
2015-07-14 09:58:10 -0600

    some functions

--- contetxt.js
+++ contetxt.js
@@ -17,6 +17,14 @@ function isOver25(person) {
   }
   return false;
 }
+function getName(person) {
+  return person.name;
+}
+function getAge(person) {
+  return person.age;
+}
 
 console.log(isOver25(aj));
 console.log(isOver25(people[1]));
+console.log(getName(people[0]));
+console.log(getAge(people[0]));
